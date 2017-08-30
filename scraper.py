4fa22c61223764a2edcb77bc93371427951a5cde
RosaEllis <31099300+RosaEllis@users.noreply.github.com>
2017-08-30 09:45:47 +0100

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -6,14 +6,17 @@
 
 import scraperwiki
 import urlparse
-import lxml.html
+import lxml.html 
 
-# scrape_table function: gets passed an individual page to scrape
+# scrape_table function: gets passed an individual page to scrape. Root is a variable just for this function (I think
+#... inside functions variables don't have to be made with the equals sign, it just knows it's a variable 
 def scrape_table(root):
+    # cssselect 
     rows = root.cssselect("table.data tr")  # selects all <tr> blocks within <table class="data">
     for row in rows:
-        # Set up our data record - we'll need it later
+        # Below sets up a dictionary that is inside the variable called record, so we can put info inside it 
         record = {}
+        # below 
         table_cells = row.cssselect("td")
         if table_cells: 
             record['Artist'] = table_cells[0].text
@@ -28,10 +31,16 @@ def scrape_table(root):
 # scrape_and_look_for_next_link function: calls the scrape_table
 # function, then hunts for a 'next' link: if one is found, calls itself again
 def scrape_and_look_for_next_link(url):
+    # below scrapes the page from the url you give it as an argument, and puts it in the variable called html
     html = scraperwiki.scrape(url)
+    # below just prints out the html
     print html
+    # below a function called fromstring, which is from the library lxml.html, takes the url which is inside the 
+    #... variable html and turns it into an xml object, and that xml object is now inside the variable called root 
     root = lxml.html.fromstring(html)
+    # below the funcion called scrape_table, which was defined above, takes the scraped html page as an xml object and carries out the function on it 
     scrape_table(root)
+    # below takes root.cssselect 
     next_link = root.cssselect("a.next")
     print next_link
     if next_link:
